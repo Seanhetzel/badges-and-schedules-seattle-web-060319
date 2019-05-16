76cd8d8cbb3b6019dfcdd38f37db89353d1b4c06
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-16 23:43:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names_array)
+  names_array.each do |name|
+    name.replace("Hello, my name is #{name}.")
+  end
+  names_array
+end
+
+def assign_rooms(speaker)
+  room = 1 
+  speaker.each do |name|
+    speaker.replace("Hello, #{name}! You'll be assigned to room #{room}!")
+    room += 1 
+  end
+  speaker
+end
