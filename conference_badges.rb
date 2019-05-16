bd823650dba4a870ac0a4ae6601aedd26506fcfb
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-16 23:54:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,11 +10,12 @@ def batch_badge_creator(names_array)
   names_array
 end
 
-def assign_rooms(speaker)
+def assign_rooms(speakers)
   room = 1 
-  speaker.each do |name|
+  speakers.each do |name|
     name.replace("Hello, #{name}! You'll be assigned to room #{room}!")
     room += 1 
   end
-  
 end
+
+def printer()
